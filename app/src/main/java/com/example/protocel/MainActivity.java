81ae7636a205d47c8d3a560890f1d3a683305386
Philippe Yu <philippejlyu@gmail.com>
2020-05-24 15:11:36 -0700

    Added Fast Android Networking

--- app/src/main/java/com/example/protocel/MainActivity.java
+++ app/src/main/java/com/example/protocel/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.protocel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.OkHttpClient;
 
 import android.os.Bundle;
+
+import com.androidnetworking.AndroidNetworking;
+import com.jacksonandroidnetworking.JacksonParserFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndroidNetworking.initialize(getApplicationContext());
+
+        OkHttpClient okHttpClient = new OkHttpClient() .newBuilder()
+                .build();
+        AndroidNetworking.initialize(getApplicationContext(),okHttpClient);
+
+        AndroidNetworking.setParserFactory(new JacksonParserFactory());
     }
 }
