aaba99f9fe568c8ed6b04de07cf3c49530622088
Cindy Fang <cindyfang70@gmail.com>
2020-07-06 17:23:53 -0400

    fixed home page layout

--- app/src/main/java/com/example/protocel/MainActivity.java
+++ app/src/main/java/com/example/protocel/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
         RetrieveProtocolTask retrieveProtocol = new RetrieveProtocolTask();
         try {
