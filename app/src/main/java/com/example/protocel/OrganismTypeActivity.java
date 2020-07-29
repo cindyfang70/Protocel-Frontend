c61ab389993d7a865e2d674412231aa65bbe13d3
Cindy Fang <cindyfang70@gmail.com>
2020-07-28 20:11:36 -0400

    recycler view

--- app/src/main/java/com/example/protocel/OrganismTypeActivity.java
+++ app/src/main/java/com/example/protocel/OrganismTypeActivity.java
@@ -1,6 +1,8 @@
 package com.example.protocel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +13,7 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class OrganismTypeActivity extends AppCompatActivity {
-
+    private ArrayList<String> organismTypes = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +23,20 @@ public class OrganismTypeActivity extends AppCompatActivity {
         try {
         RetrieveProtocolTask retrieveProtocol = new RetrieveProtocolTask();
         JSONObject data = retrieveProtocol.execute(login_info).get();
+
         }  catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException err) {
             err.printStackTrace();
         }
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.organism_recycler_view);
+        organismTypes.add("Prokaryotes");
+        organismTypes.add("Eukaryotes");
+        ProtocolAdapter adapter = new ProtocolAdapter(this, organismTypes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/protocel/ProtocolAdapter.java
+++ app/src/main/java/com/example/protocel/ProtocolAdapter.java
@@ -1,37 +1,59 @@
 package com.example.protocel;
 
+import android.content.Context;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.protocel.ProtocolHolder;
 
-public class ProtocolAdapter extends RecyclerView.Adapter {
-    private String[] mDataset;
-    public ProtocolAdapter(String[] myDataset){
-        mDataset = myDataset;
+import java.util.ArrayList;
+
+public class ProtocolAdapter extends RecyclerView.Adapter<ProtocolAdapter.ViewHolder> {
+
+    private ArrayList<String> protocols = new ArrayList<String>();
+    private Context mContext;
+    public ProtocolAdapter(Context context, ArrayList<String> protocols){
+        mContext = context;
+        protocols = protocols;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.protocol_listitem, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public ProtocolHolder onCreateViewHolder(ViewGroup parent, int viewtype){
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.protocol_row_layout, parent, false);
-        ProtocolHolder ph = new ProtocolHolder(v);
-        return ph;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.parentLayout.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d("LISTITEM", "clicked");
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return protocols.size();
     }
 
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView protocolName;
+        RelativeLayout parentLayout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+            protocolName = itemView.findViewById(R.id.prokaryotes_text);
+        }
+    }
 }
