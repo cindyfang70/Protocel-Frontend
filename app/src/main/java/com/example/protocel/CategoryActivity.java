f02d445666c2e660d21018c4ebd5863351092677
Cindy Fang <cindyfang70@gmail.com>
2020-09-12 16:48:35 -0400

    added upper toolbar to activities

--- app/src/main/java/com/example/protocel/CategoryActivity.java
+++ app/src/main/java/com/example/protocel/CategoryActivity.java
@@ -2,18 +2,31 @@ package com.example.protocel;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -29,6 +42,10 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
         Intent intent = getIntent();
+
+        //setting up the upper toolbar
+        Toolbar myToolbar = findViewById(R.id.category_toolbar);
+        setSupportActionBar(myToolbar);
 
         //setting up the bottom navigation bar
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation_view);
@@ -82,5 +99,58 @@ public class CategoryActivity extends AppCompatActivity {
             recyclerView.setAdapter(adapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(this));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.sort_protocols, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        ReadLoginFromFile readLogin = new ReadLoginFromFile();
+        String info = readLogin.readFromFile(CategoryActivity.this);
+        String[] split_info = info.split(":");
+        String username = split_info[0];
+        username = username.replace("\n", "");
+        String token = split_info[1];
+        switch (item.getItemId()) {
+            case R.id.action_sort:
+                OkHttpClient sort_client = new OkHttpClient();
+                RequestBody sort_requestBody = new FormBody.Builder()
+                        .build();
+                Request sort_request = new Request.Builder()
+                        .header("token", token)
+                        .header("username", username)
+                        .url("https://www.philippeyu.ca/like_protocol")
+                        .post(sort_requestBody)
+                        .build();
+                sort_client.newCall(sort_request).enqueue(new Callback() {
+                    Handler mainHandler = new Handler(CategoryActivity.this.getMainLooper());
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        mainHandler.post(new Runnable(){
+                            @Override
+                            public void run() {
+                                Toast.makeText(CategoryActivity.this, "Sort good", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        mainHandler.post(new Runnable(){
+                            @Override
+                            public void run() {
+                                Toast.makeText(CategoryActivity.this, "Sort bad", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                return true;
+
+
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/protocel/OrganismTypeActivity.java
+++ app/src/main/java/com/example/protocel/OrganismTypeActivity.java
@@ -2,24 +2,36 @@ package com.example.protocel;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
@@ -61,6 +73,10 @@ public class OrganismTypeActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+            //setting up the upper toolbar
+            Toolbar myToolbar = findViewById(R.id.organismtype_toolbar);
+            setSupportActionBar(myToolbar);
 
         // Iterate through all the keys for the top level of the data, and add the categories to it
         for (Iterator<String> it = allData.keys(); it.hasNext(); ) {
@@ -106,5 +122,58 @@ public class OrganismTypeActivity extends AppCompatActivity {
         OrganismAdapter adapter = new OrganismAdapter(this, organismTypes);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.sort_protocols, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        ReadLoginFromFile readLogin = new ReadLoginFromFile();
+        String info = readLogin.readFromFile(OrganismTypeActivity.this);
+        String[] split_info = info.split(":");
+        String username = split_info[0];
+        username = username.replace("\n", "");
+        String token = split_info[1];
+        switch (item.getItemId()) {
+            case R.id.action_sort:
+                OkHttpClient sort_client = new OkHttpClient();
+                RequestBody sort_requestBody = new FormBody.Builder()
+                        .build();
+                Request sort_request = new Request.Builder()
+                        .header("token", token)
+                        .header("username", username)
+                        .url("https://www.philippeyu.ca/like_protocol")
+                        .post(sort_requestBody)
+                        .build();
+                sort_client.newCall(sort_request).enqueue(new Callback() {
+                    Handler mainHandler = new Handler(OrganismTypeActivity.this.getMainLooper());
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        mainHandler.post(new Runnable(){
+                            @Override
+                            public void run() {
+                                Toast.makeText(OrganismTypeActivity.this, "Sort good", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        mainHandler.post(new Runnable(){
+                            @Override
+                            public void run() {
+                                Toast.makeText(OrganismTypeActivity.this, "Sort bad", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+                return true;
+
+
+        }
+        return true;
     }
 }
